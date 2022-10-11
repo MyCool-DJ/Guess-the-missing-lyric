8212811d081f89a65ffd07e2bd000071d1c09f8f
Michael Spicer <>
2022-10-11 04:29:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#day 16 of 100-Days-Of-Code
+while True:
+    print("This ____ has taken its toll on me...")
+    missinglyric = input("Enter the missing lyric: ")
+    if missinglyric == "love":
+        break
+
+print("You did it!")
